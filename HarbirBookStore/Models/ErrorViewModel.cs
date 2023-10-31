2d86324d60a5d2524746a3f54726e081dc823d5b
W0825064 <W0825064@ADL-A1201I-17>
2023-10-30 21:11:05 -0400

    Add project files.

---
+++ HarbirBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HarbirBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
