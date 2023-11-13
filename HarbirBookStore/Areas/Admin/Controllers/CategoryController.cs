76a420376c36bb3ef015112c641bfdf106b8ae26
W0825064 <W0825064@ADL-A1201K-23>
2023-11-13 17:49:39 -0500

    modify the CategoryController.cs and create a new folder name-Category

--- HarbirBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ HarbirBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HarbirBooks.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,30 @@ using System.Threading.Tasks;
 
 namespace HarbirBookStore.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
+        private readonly IUnitOfWork _UnitOfWork;
+
+        public CategoryController(IUnitOfWork UnitOfWork)
+        {
+            _UnitOfWork = UnitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        // API calls here
+        #region API CALLS
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            //return NotFound();
+            var allObj = _UnitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
     }
 }
