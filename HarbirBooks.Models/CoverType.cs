8d054867bf899701555055a14361eeaa0e15e4dd
W0825064 <W0825064@ADL-A1201G-13>
2023-11-20 21:28:37 -0500

    fixed the error which was occuring inside the Upsert.cshtml

--- HarbirBooks.Models/CoverType.cs
+++ HarbirBooks.Models/CoverType.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace HarbirBooks.Models.ViewModels
+namespace HarbirBooks.Models
 {
     public class CoverType
     {
